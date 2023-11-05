93b45b26623c0ab6e7c8d5e46bcb0a23beb5a179
Bagus Sannto <sananto@ptmitrabhakti.com>
2023-11-05 20:51:22 +0700

    FIxing autenticate and language RU

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Session; // Import the Session facade
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +21,29 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            $user = null;
+
+            // Access the user from the session
+            if (Session::has('user')) {
+                $user = Session::get('user');
+                $username = $user->user_id;
+                $usernewUpdate = DB::connection('member')
+                    ->table('dbo.GM_MEMBER')
+                    ->where('user_id', $username)
+                    ->first(); // Use 'first' to retrieve a single record
+
+                if ($usernewUpdate) {
+                    session()->forget('user');
+                    // Update the 'cash' attribute in the user's session
+                    session()->put('user', $usernewUpdate);
+                    Log::info("SUCESS UPDATE SESSION");
+                }
+            }
+
+            $view->with('user', $user);
+        });
     }
 }
